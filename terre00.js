47e99c6832b8e1267ff9e6df03bd8ac93a595a4d
Ayoub Ammouche <ayoubammouche1@gmail.com>
2024-05-22 12:14:01 +0200

    alphabet fini

--- terre00.js
+++ terre00.js
@@ -1,5 +1,7 @@
-let alphabet = "abcde";
+let output = "";
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
 for (let i = 0; i < alphabet.length; i++) {
   let boucle = alphabet[i];
-  console.log(boucle);
+  output = output + "" + boucle;
 }
+console.log(output);
