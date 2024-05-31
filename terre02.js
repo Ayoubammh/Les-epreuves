72e72b3e163290564c4f3f6e6736446abec516b0
Ayoub Ammouche <ayoubammouche1@gmail.com>
2024-05-31 12:58:36 +0200

    boucle argv

--- terre02.js
+++ terre02.js
@@ -1,6 +1,9 @@
-let arguments = "Je suis un codeur !";
-const mots = arguments.split(" ");
-console.log(arguments);
-for (let i = 0; i < mots.length; i++) {
-  console.log(mots[i]);
+let { argv } = require("process");
+
+let mot = argv.splice(2);
+let phrase = mot;
+
+for (let i = 0; i < phrase.length; i++) {
+  let coupage = phrase[i];
+  console.log(coupage);
 }
