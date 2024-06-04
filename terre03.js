6cb4467099503bf351cc6709132b687aff09b114
Ayoub Ammouche <ayoubammouche1@gmail.com>
2024-06-04 10:16:55 +0200

    alphabet à partir de fini

--- terre03.js
+++ terre03.js
@@ -1,18 +1,18 @@
 //Recupere l'argument
-let { argv } = require("process");
-let letter = argv.splice(2);
+const { argv } = require("process");
+const letter = argv.splice(2);
 
 //Convertir l'argument en ASCII
-let asciiValues = letter.map((word) =>
+const asciiValues = letter.map((word) =>
   word.split("").map((char) => char.charCodeAt(0))
 );
 
 //Converti le tableau en number
-let asciiTrueValue = asciiValues.toString();
-let asciiNumberValue = Number(asciiTrueValue);
+const asciiTrueValue = asciiValues.toString();
+const asciiNumberValue = Number(asciiTrueValue);
 //console.log(asciiNumberValue);
 
-let lastLetter = 123;
+const lastLetter = 123;
 let result = "";
 
 for (let i = asciiNumberValue; i < lastLetter; i++) {
