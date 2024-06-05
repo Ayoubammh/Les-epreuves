de8e3858d6a65c175cf38f307bea5d9c3632191b
Ayoub Ammouche <ayoubammouche1@gmail.com>
2024-06-05 23:06:49 +0200

    Division fini

---
+++ terre05.js
@@ -0,0 +1,22 @@
+const { argv, loadEnvFile } = require("process");
+
+//recupérer les 2 arguments et les transformer en number
+const firstNumber = Number(argv[2]);
+const secondNumber = Number(argv[3]);
+
+//diviser les arguments et leurs restes
+const resultat = firstNumber / secondNumber;
+const reste = firstNumber % secondNumber;
+
+//fonction qui affiche le resultat des arguments diviser en fonction des attentes
+if (firstNumber > secondNumber && reste > 0) {
+  console.log(`resultat : ${resultat}`);
+  console.log(`reste : ${reste}`);
+} else if (
+  firstNumber < secondNumber ||
+  reste === 0 ||
+  isNaN(resultat) ||
+  secondNumber == 0
+) {
+  console.log("erreur. ");
+}
