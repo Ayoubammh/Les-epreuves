1b6b5fc52b8fba45792e5ef2d86186c211dfc1dc
Ayoub Ammouche <ayoubammouche1@gmail.com>
2024-06-06 10:40:27 +0200

    Iverser chaine fini

---
+++ terre06.js
@@ -0,0 +1,19 @@
+const { argv } = require("process");
+
+//récuperer l'argument et l'affecter en string
+const stringRedress = String(argv[2]);
+//console.log(stringRedress);
+
+//Boucle qui inverse l'argument string
+
+let reverse = "";
+for (let i = stringRedress.length - 1; i >= 0; i--) {
+  reverse += stringRedress[i];
+}
+
+//Fonction qui gere les erreur si l'argument et un number ou vide
+if (stringRedress == reverse.toString() || stringRedress === "undefined") {
+  console.log("Erreur");
+} else {
+  console.log(reverse);
+}
