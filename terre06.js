129f0425950042eeb74cb47992fa70b45fa904e8
Ayoub Ammouche <ayoubammouche1@gmail.com>
2024-11-14 08:13:46 +0100

    Inversion fini

--- terre06.js
+++ terre06.js
@@ -1,23 +1,22 @@
+const { log } = require("console");
 const { argv } = require("process");
 
 //récuperer l'argument et l'affecter en string
-const stringRedress = String(argv[2]);
-//console.log(stringRedress);
-
-//Boucle qui inverse l'argument string
-
+const reverseString = String(argv[2]);
+const quotationMarks = [
+  String.fromCodePoint(34) + reverseString + String.fromCodePoint(34),
+];
 let reverse = "";
-for (let i = stringRedress.length - 1; i >= 0; i--) {
-  reverse += stringRedress[i];
-}
 
 //Fonction qui gere les erreur si l'argument et un number ou vide
 if (
-  stringRedress == reverse.toString() ||
-  stringRedress === "undefined" ||
-  stringRedress === reverse
+  (quotationMarks && Number(reverseString)) ||
+  (reverseString !== reverse.toString() && reverseString === "undefined")
 ) {
-  console.log("Erreur");
+  console.log("Erreur.");
 } else {
+  for (let i = reverseString.length - 1; i >= 0; i--) {
+    reverse += reverseString[i];
+  }
   console.log(reverse);
 }
